457625e2d3d8c9d8382d59dd42c807aea64a75fb
giada <giadabrescia2006@gmail.com>
2024-06-03 13:42:19 +0200

    sistemo

--- cerca-prodotti.php
+++ cerca-prodotti.php
@@ -30,6 +30,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<title>Ricerca prodotto</title>
 	<link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" href="cerca.css">
 </head>
 <body>
     <?php require("pagine/header.php");?>
